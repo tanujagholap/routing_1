c6673301c74cc09128b776e20710d75cd74c4c3a
demo1 <tanujarbhong@gmail.com>
2024-04-15 12:21:03 +0530

    initial commit

---
+++ src/compo/Errorpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Errorpage = () => {
+  return (
+    <div>
+      <h2>Error</h2>
+      <img src="C:\reactjs\routing\src\error404.jpg" alt='Error Imgae'></img>
+    </div>
+  )
+}
+
+export default Errorpage
